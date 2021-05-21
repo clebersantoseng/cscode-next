9455a0c2540b71a4584ea8bbccb0402cfd50fce2
Cleber Santos <clebersantoseng@outlook.com>
2021-05-20 22:38:43 -0300

    Alteração pagina home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { FaChartPie, FaLaptopCode, FaShieldAlt } from 'react-icons/fa';
+
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -15,12 +18,43 @@ export default function Home() {
         <div className={styles.wrapper}>
           <section>
             <div className={styles.slideText}>
-              <h1 className="animate__animated animate__backInLeft">Gestão Comercial</h1>
-              <p className=" animate__animated animate__fadeInLeft animate__delay-1s">Facilite a sua produção, automatize sua empresa !</p>
+              <h1 className="animate__animated animate__faster animate__backInLeft">Gestão Comercial</h1>
+              <p className=" animate__animated animate__slow animate__fadeInLeft">Facilite a sua produção, automatize sua empresa !</p>
             </div>
 
             <img src="/image/dashboard.png" alt="Computador" />
           </section>
+
+          <aside>
+            <div className={styles.wrapperGrid}>
+              <div className={styles.gridItem}>
+                <div className={styles.gridText}>
+                  <h1><FaShieldAlt /> Assistência Técnica</h1>
+                  <p>Trabalhamos com técnicos e especialistas formados em áreas tecnólogicas, que amam o que fazem, e se empenham para resolver seu problema da melhor e mais rapida forma possivel.</p>
+                </div>
+
+                <Link href="#">Saiba mais</Link>
+              </div>
+
+              <div className={styles.gridItem}>
+                <div className={styles.gridText}>
+                  <h1><FaChartPie /> Gerenciamento</h1>
+                  <p>Disponibilizamos as melhores práticas de gestão atraves de softwares e serviços que ajuda voce a empreender e tomar decições que seu negócio precisa.</p>
+                </div>
+
+                <Link href="#">Saiba mais</Link>
+              </div>
+
+              <div className={styles.gridItem}>
+                <div className={styles.gridText}>
+                <h1><FaLaptopCode />Sob Medida</h1>
+                <p>Uma empresa especializada em desenvolvimento web, como criação de sites, sistemas web e aplicativos mobile, sob medida de acordo com que sua empresa necessita.</p>
+                
+                </div>
+                <Link href="#">Saiba mais</Link>
+              </div>
+            </div>
+          </aside>
         </div>
 
       </div>
